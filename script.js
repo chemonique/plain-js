9db058a1aee9eff1d2792758e80931dd39308439
chemonique <122254404+chemonique@users.noreply.github.com>
2023-08-27 21:14:16 +0200

    everthing but fav

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-const showListContainer = document.getElementById('show-list');
+ const showListContainer = document.getElementById('show-list');
  const showDetailsContainer = document.getElementById('show-details');
  const seasonSelectorContainer = document.getElementById('season-selector');
+ 
  const favoritesContainer = document.getElementById('favorites');
  const favoritesSortSelect = document.getElementById('favorites-sort-select');
  const favoriteEpisodesContainer = document.getElementById('favorite-episodes');
+ 
  const searchInput = document.getElementById('search-input');
  const sortSelect = document.getElementById('sort-select');
+ 
  const audio = document.getElementById('audio');
  const audioProgress = document.getElementById('audio-progress');
+ 
  let currentShowId = null; // To track the current show being viewed
  let favoriteEpisodes = [];
  let showsData = [];
@@ -47,11 +51,13 @@ const showListContainer = document.getElementById('show-list');
        const details = `
          <h2>${show.title}</h2>
          <p>${show.description}</p>
-       `;
+       
+         <button onclick="fetchShowSeasons(${showId})">View Seasons</button>
+                `;
 
        showDetailsContainer.innerHTML = details;
-
-       fetchShowSeasons(showId);
+      
+       
      })
      .catch(error => {
        showDetailsContainer.innerHTML = 'Error loading show details.';
@@ -63,15 +69,17 @@ const showListContainer = document.getElementById('show-list');
  function fetchShowSeasons(showId) {
    showLoading(seasonSelectorContainer);
 
-   fetch(`https://podcast-api.netlify.app/id/${showId}/seasons`)
+   fetch(`https://podcast-api.netlify.app/id/${showId}`)
      .then(response => response.json())
-     .then(seasons => {
-       const seasonItems = seasons.map(season => `
+     .then(seasons=> {
+       
+       const seasonItems = seasons.seasons.map(season => `
          <div>
-           <img src="${season.image}" alt="Season ${season.number}">
-           <h3>Season ${season.number}</h3>
-           <p>Episodes: ${season.episodes}</p>
-           <button onclick="fetchSeasonEpisodes(${showId}, ${season.number})">View Episodes</button>
+           <img src="${season.image}" alt="Season ${season.season}">
+           <h3>Season ${season.season}</h3>
+           <h4> ${season.title}
+           <p>Episodes: ${season.episodes.length}</p>
+           <button onclick="fetchSeasonEpisodes(${showId}, ${season.season})">View Episodes</button>
          </div>
        `).join('');
 
@@ -87,23 +95,28 @@ const showListContainer = document.getElementById('show-list');
  function fetchSeasonEpisodes(showId, seasonNumber) {
    showLoading(showDetailsContainer);
 
-   fetch(`https://podcast-api.netlify.app/shows/${showId}/seasons/${seasonNumber}/episodes`)
+   fetch(`https://podcast-api.netlify.app/id/${showId}`)
      .then(response => response.json())
-     .then(episodes => {
+     .then(episode => {
+     const {episodes} = episode.seasons[seasonNumber-1]
        const episodeList = episodes.map(episode => `
          <div>
-           <p>Episode ${episode.number}: ${episode.title}</p>
+           <p>Episode ${episode.episode}: ${episode.title}</p>
            <audio controls>
-             <source src="${episode.audio}" type="audio/mpeg">
+             <source src="${episode.file}" type="audio/mpeg">
              Your browser does not support the audio element.
            </audio>
+           <button onclick="toggleFavorite(${episode.episode})">Back to Show</button>
          </div>
        `).join('');
+       console.log(episode)
 
+      
        const seasonDetails = `
          <button onclick="fetchShowDetails(${currentShowId})">Back to Show</button>
          <h3>Season ${seasonNumber}</h3>
          ${episodeList}
+         
        `;
 
        showDetailsContainer.innerHTML = seasonDetails;
@@ -117,15 +130,16 @@ const showListContainer = document.getElementById('show-list');
  // Toggle episode as favorite
  function toggleFavorite(episode) {
    const episodeIndex = favoriteEpisodes.findIndex(favEpisode => 
-     favEpisode.id === episode.id && favEpisode.season === episode.season
+     favEpisode.id === episode && favEpisode.season === episode.season
    );
-
+   
    if (episodeIndex === -1) {
      favoriteEpisodes.push(episode);
-   } else {
-     favoriteEpisodes.splice(episodeIndex, 1);
-   }
-
+    } 
+  //else {
+  //     favoriteEpisodes.push(episode);
+  //   }
+   console.log(favoriteEpisodes)
    updateFavoritesView();
  }
 
@@ -173,18 +187,18 @@ const showListContainer = document.getElementById('show-list');
    } else if (sortBy === 'title-desc') {
      filteredShows.sort((a, b) => b.title.localeCompare(a.title));
    } else if (sortBy === 'date-asc') {
-     filteredShows.sort((a, b) => new Date(a.lastUpdated) - new Date(b.lastUpdated));
+     filteredShows.sort((a, b) => new Date(a.updated) - new Date(b.updated));
    } else if (sortBy === 'date-desc') {
-     filteredShows.sort((a, b) => new Date(b.lastUpdated) - new Date(a.lastUpdated));
+     filteredShows.sort((a, b) => new Date(b.updated) - new Date(a.updated));
    }
 
    const showItems = filteredShows.map(show => `
-     <div>
+     <div class = 'sigleshow'>
        <img src="${show.image}" alt="${show.title}">
        <h2>${show.title}</h2>
        <p>Genres: ${show.genres.join(', ')}</p>
        <p>Seasons: ${show.seasons}</p>
-       <p>Last Updated: ${new Date(show.lastUpdated).toDateString()}</p>
+       <p>Last Updated: ${new Date(show.updated).toLocaleDateString()}</p>
        <button onclick="fetchShowDetails(${show.id})">View Details</button>
      </div>
    `).join('');
